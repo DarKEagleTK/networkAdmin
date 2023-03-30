7a7ed947e18d20cf0e3dec4b424f468af2359d09
Dimitri <dimitrilepoutre7@gmail.com>
2023-03-31 00:55:11 +0200

    nombre q'equipment configurer

--- routes/configurateur.js
+++ routes/configurateur.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('configurateur');
+    const mode = 0;
+    res.render('configurateur', {mode: mode});
+})
+
+router.post('/nbr', (req, res, next) => {
+    const nombre_equipment = req.body.nbrequipment;
+    console.log(nombre_equipment);
+    const mode = 1;
+    res.render('configurateur', {mode: mode, nbrequipment: nombre_equipment});
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 8080;
+const bodyParser = require('body-parser');
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes
 var indexRouter = require('./routes/index');
@@ -16,6 +21,7 @@ var confRouter = require('./routes/configurateur');
 app.use('/', indexRouter);
 app.use('/documentation', docRouter);
 app.use('/configurateur', confRouter);
+
 
 //app
 app.listen(port, () => {
